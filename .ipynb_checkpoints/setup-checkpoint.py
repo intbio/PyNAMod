d506e473434fdb389375078dafc78ce88607a8a3
veniamin-vasilev <82019295+veniamin-vasilev@users.noreply.github.com>
2025-04-23 17:14:13 +0300

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-setuptools.setup(     
-     name="pynamod",     
-     version="0.1",
-     python_requires=">=3.11",   
-     packages=setuptools.find_packages(exclude=['examples']),
-     data_files=['pynamod/atomic_analysis/classifier.pkl'],
-     include_package_data=True
-)
