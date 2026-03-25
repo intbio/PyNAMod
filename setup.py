2eb65e741480c0ceef0c543b1c3fe6fa463d62ee
Vladimir Sidorov <VladimirSidorov90000@gmail.com>
2026-03-25 08:36:41 +0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="pynamod",
     version="0.1",
-    python_requires="==3.13",
+    python_requires=">=3.13,<3.14",
     packages=setuptools.find_packages(exclude=['examples']),
     data_files=['pynamod/atomic_analysis/classifier.pkl', 'pynamod/tests/cg_3lz0.h5'],
     include_package_data=True
