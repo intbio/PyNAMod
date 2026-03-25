28e71d3cbf27fa88f9ed72cfbc36e9f06a7d7cd8
Vladimir Sidorov <VladimirSidorov90000@gmail.com>
2026-03-25 08:23:42 +0300

    Suppressed pkg_resources deprecation warning

--- pynamod/__init__.py
+++ pynamod/__init__.py
@@ -1,11 +1,10 @@
 import warnings
-
 import pypdb
+
+warnings.filterwarnings('ignore', category=UserWarning)
+warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 import pynamod.geometry
 from pynamod.energy.energy import Energy
 from pynamod.MC_simulation.iterator import Iterator
 from pynamod.structures import *
-
-warnings.filterwarnings('ignore', category=UserWarning)
-warnings.filterwarnings('ignore', category=DeprecationWarning)
