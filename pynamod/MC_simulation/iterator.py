Fixed bug with pair params sign.

pynamod/MC_simulation/iterator.py
@@ -1,11 +1,8 @@
-import signal
-
-import numpy as np
 import torch
-
-from pynamod.geometry.trajectories import H5_Trajectory, Tensor_Trajectory
-from pynamod.MC_simulation.rotation_handler import _Rotation_Handler
+import signal
+from pynamod.geometry.trajectories import Tensor_Trajectory
 from pynamod.MC_simulation.stats_display import _Stats_Display
+from pynamod.MC_simulation.rotation_handler import _Rotation_Handler
 
 
 class Iterator:
@@ -16,27 +13,54 @@ class Iterator:
 
         self.res_trajectory = cg_structure.dna.geom_params.trajectory
 
-    def run(self, target_accepted_steps=int(1e5), max_steps=int(1e6), transfer_to_memory_every=None, save_every=1,
-            mute=False, KT_factor=1, integration_mod='minimize', device='cpu', traj_init_step=None):
+    def run(self, target_accepted_steps=int(1e5), max_steps=int(1e6), transfer_to_memory_every=None, save_every=1, rebuild_every=400,
+            KT_factor=1, mute=False, integration_mode='minimize', device='cpu', traj_init_step=None, dtype=torch.float,debug = False,output=None):
+        '''Starts Monte Carlo Simulation.
+    
+            Attributes:
+    
+            - **target_accepted_steps** - number of accepted simulation steps to reach, default 1e5.
+    
+            - **max_steps** - maximum number of attempted steps. If reached run will be terminated even if target_accepted_steps was not matched. Default 1e6.
+    
+            - **transfer_to_memory_every** - When using gpu, accepted frames will be flushed to memory or h5 file (depending on trajectory of CG structure) each chosen number of accepted steps. If None will flush only at the end of the simulation. Default None.
+    
+            - **save_every** - Accepted frames will be saved in resulting trajectory every chosen number of accepted steps. Default 1.
 
-        self._prepare_system(target_accepted_steps, transfer_to_memory_every, device, integration_mod, traj_init_step, KT_factor)
+            - **rebuild_every** - Full procedure of rebuilding from bp step paramaters will be used every chosen number of accepted steps instead of simplified rotation and shift. This is necessary to negate computing error. The required frequency can be determined using debug (see debug attribute). Default 400.
+
+            - **KT_factor** - factor that will be used for KT from pynamod.Energy object in Metropolis criterion. Defailt 1.
+
+            - **mute** - If False, tqdm bars will be used to show simulation data, if True instead every 5 minutes number of accepted steps and acceptance rate will be printed. Default False.
+
+            - **integration_mode** - 'minimize' or 'random_step'. If minimize, Iterator will go through movable bp steps repeatedly using one of them as a changing bp step. If 'random_step', Iterator will choose a bp step to change randomly from movable bp steps. Default 'minimize'.
+
+            - **device** - pytorch style 'cpu' or 'cuda' ('cuda:n'). Device to use for simulations. Default 'cpu'.
+
+            - **traj_init_step** - If not None, frame from this trajectory step will be used as an initial conformation. If None, current step from trajectory is used. Default None.
+
+            - **dtype** - tensor dtype to use in calculations. Default torch.float.
+
+            - **debug** - 
+        '''
+        self._prepare_system(target_accepted_steps, transfer_to_memory_every, rebuild_every,
+                             device, integration_mode, traj_init_step, KT_factor, dtype, debug)
 
         self._stop_loop = False
         signal.signal(signal.SIGINT, self._signal_handler)
-
-        self._stats_display = _Stats_Display(max_steps, mute)
+        self._stats_display = _Stats_Display(max_steps,mute,output)
 
         while self.total_step < max_steps and self.accepted_steps < target_accepted_steps:
 
             self._integration_step(save_every, integration_mode)
 
             self._stats_display.show_step_data(self.accepted_steps, self.total_step, self.prev_e.sum().item())
-            # Stop only when a step is completed in case of keyboard interrupt.
+            #Stop only when a step is completed in case of keyboard interrupt.
             if self._stop_loop:
                 break
 
         self._transfer_to_memory(steps=self.trajectory.cur_step)
-        self._stats_display.show_final_data(target_accepted_steps, self._stop_loop, self.accepted_steps, self.total_step)
+        self._stats_display.show_final_data(target_accepted_steps,self._stop_loop,self.accepted_steps,self.total_step)
 
     def to(self, device):
         self.trajectory.to(device)
@@ -61,7 +85,10 @@ class Iterator:
             traj_init_step = len(self.res_trajectory) - 1
         cur_step = self.res_trajectory.cur_step
         self.res_trajectory.cur_step = traj_init_step
-        self._create_tens_trajectory()
+        self._create_tens_trajectory(dtype)
+        if 'rlsp_origins' not in self.res_trajectory.attrs_names:
+            self.res_trajectory.add_attr('rlsp_origins',self.trajectory.rlsp_origins.shape)
+            self.res_trajectory.rlsp_origins = self.trajectory.rlsp_origins
         self.to(device)
 
         if 'energies' not in self.res_trajectory.attrs_names:
@@ -77,13 +104,10 @@ class Iterator:
         self._scaled_KT = KT_factor*self.energy.KT
 
     def _set_change_indices(self, integration_mode):
-
         self.movable_ind = torch.arange(self.trajectory.data_len, dtype=int)[self.cg_structure.dna.movable_steps]
-        if self.movable_ind.shape[0] > 0 and self.movable_ind[0] == 0:
+        if self.movable_ind[0] == 0:
             self.movable_ind = self.movable_ind[1:]
-        if self.movable_ind.shape[0] == 0:
-            raise ValueError('No movable DNA steps available for MC simulation. Set cg_structure.dna.movable_steps[1:] = True or build/analyze DNA with movable=True.')
-        if integration_mod == 'minimize':
+        if integration_mode == 'minimize':
             self.cur_movable_ind = 0
         elif integration_mode == 'random_step':
             self.movable_ind_len = self.movable_ind.shape[0]
@@ -98,7 +122,7 @@ class Iterator:
         traj_len = self.transfer_to_memory_every + 1
         dtype = init_ref_frames.dtype
 
-        self.trajectory = Tensor_Trajectory(dtype, traj_len, ln, torch.tensor, attrs_names=['prot_origins'], shapes=[(traj_len, init_prot_ori.shape[0], 1, 3)])
+        self.trajectory = Tensor_Trajectory(dtype,traj_len,ln,torch.tensor,attrs_names=['rlsp_origins'],shapes=[(traj_len,init_rlsp_ori.shape[0],1,3)])
         self.trajectory.origins, self.trajectory.ref_frames = init_ori, init_ref_frames
         self.trajectory.local_params = init_local_params
         self.trajectory.rlsp_origins = init_rlsp_ori
@@ -124,7 +148,7 @@ class Iterator:
         r = torch.rand(1).item()
         self.total_step += 1
 
-        if not Del_E.isnan() and Del_E < 0 or (not (torch.isinf(torch.exp(Del_E))) and (r <= torch.exp(-Del_E/self._scaled_KT))):
+        if not Del_E.isnan() and Del_E < 0 or (not (torch.isinf(torch.exp(Del_E))) and (r  <= torch.exp(-Del_E/self._scaled_KT))):
             self.energy.update_matrices(e_mat, s_mat, change_indices[1])
             self.prev_e += e_dif_components
             self.accepted_steps += 1
@@ -166,12 +190,12 @@ class Iterator:
             self.cur_movable_ind += 1
             if self.cur_movable_ind == self.movable_ind.shape[0]:
                 self.cur_movable_ind = 0
-        elif integration_mod == 'random_step':
+        elif integration_mode == 'random_step':
             cur_index = torch.randint(self.movable_ind_len, (1,))
 
-        dna_change_index = self.movable_ind[cur_index]
-        prot_change_index = sum([protein.n_cg_beads for protein in self.cg_structure.proteins if protein.ref_pair.ind < dna_change_index])
-        return dna_change_index, prot_change_index
+        ref_ori_change_index = self.movable_ind[cur_index]
+        rlsp_change_index = sum([group.n_cg_beads for group in self.cg_structure.rlsp_groups if group.ref_pair.ind < ref_ori_change_index])
+        return ref_ori_change_index,rlsp_change_index
 
     def _transfer_to_memory(self, steps=None):
         if steps is None:
@@ -186,5 +210,6 @@ class Iterator:
 
         for i in range(steps):
             self.res_trajectory.cur_step += 1
-            self.res_trajectory.add_frame(self.res_trajectory.cur_step, origins=origins_traj[i], ref_frames=ref_frames_traj[i],
-                                          local_params=local_params_traj[i], energies=energy_comp_traj[i])
+            self.res_trajectory.add_frame(self.res_trajectory.cur_step, origins=origins_traj[i],
+                                          ref_frames=ref_frames_traj[i], local_params=local_params_traj[i],
+                                          rlsp_origins=rlsp_origins_traj[i], energies=energy_comp_traj[i])

pynamod/atomic_analysis/pairs_parser.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import numpy as np
-import torch
-import pickle
-from itertools import combinations
-from scipy.spatial.transform import Rotation as R
-from scipy.spatial.distance import pdist
-from pynamod.geometry.geometrical_parameters import Geometrical_Parameters
-from pynamod.atomic_analysis.structures_storage import Pairs_Storage
-
-=======
->>>>>>> 4c2722a3fcadace9ef261cd86d95e432a14f8376
 from importlib.resources import files
 from itertools import combinations
 
@@ -56,28 +44,27 @@ classifier = _OnnxPairClassifier(_onnx_path.read_bytes())
 
 class Base_Pair:
     '''
-    Represents data of a single base pair. It exists as a reference to a pairs storage object which contains data of all pairs in a structure. All of its main
+    Represents data of a single base pair. It exists as a reference to a pairs storage object which contains data of all pairs in a structure. All of its main 
     variables are properties that get information from the storage class or set it there. These variables include:
-
+    
     **lead_nucl**, **lag_nucl** - Nucleotide objects.
-
+    
     **lead_nucl_ind**, **lag_nucl_ind** - indices to locate pair nucleotides in nucleotide storage.
-
+    
     **pair_name** - string, consists of sorted types of Nucleotides in this pair.
-
+    
     **radius**, **charge**, **epsilon** - parameters of a pair for energy functions.
-
+    
     **geom_params** - Geometrical_Parameters object that stores local geometrical parameters of a pair as well as reference middle frame and origin of a pair. Note that this is different from step parameters, as it calculates parameters between two nucleotides.
-
+    
     **pair_params**, **Rm**, **om** - torch Tensors of pair geometrical parameters, reference middle frame and origin.
-
+    
     '''
-
-    def __init__(self, storage_class, lead_nucl_ind=None,
-                 lag_nucl_ind=None, ind=None, lead_nucl=None, lag_nucl=None, radius=10, charge=-2, eps=0.5, geom_params=None):
+    def __init__(self, storage_class,lead_nucl_ind=None,
+                 lag_nucl_ind=None,ind=None,lead_nucl=None,lag_nucl=None,radius=10,charge=-2,eps=0.5,geom_params=None):
         self.storage_class = storage_class
         self.ind = ind
-
+        
         if lead_nucl:
             lead_nucl_ind = lead_nucl.ind
             lag_nucl_ind = lag_nucl.ind
@@ -85,59 +72,59 @@ class Base_Pair:
         if ind is None:
             ind = len(storage_class)
             self.ind = ind
-            storage_class.append(lead_nucl_ind, lag_nucl_ind, radius, charge, eps, geom_params)
+            storage_class.append(lead_nucl_ind,lag_nucl_ind,radius,charge,eps,geom_params)
 
             if not storage_class.nucleotides_storage[lead_nucl_ind].leading_strand:
                 self.lead_nucl_ind, self.lag_nucl_ind = self.lag_nucl_ind, self.lead_nucl_ind
 
         else:
             lead_nucl_ind = storage_class.lead_nucl_inds[ind]
-            lag_nucl_ind = storage_class.lag_nucl_inds[ind]
-
+            lag_nucl_ind = storage_class.lag_nucl_inds[ind]   
+        
     def __lt__(self, other):
         return self.lead_nucl.__lt__(other.lead_nucl)
-
-    def __eq__(self, other):
+    
+    def __eq__(self,other):
         return self.storage_class == other.storage_class and self.ind == other.ind
-
+    
     def __repr__(self):
-        return f'<Nucleotides pair with resids {self.lead_nucl.resid}, {self.lag_nucl.resid}, and segids {self.lead_nucl.segid}, {self.lag_nucl.segid}>'
-
+        return f'<Nucleotides pair with resids {self.lead_nucl.resid}, {self.lag_nucl.resid}, and segids {self.lead_nucl.segid}, {self.lag_nucl.segid}>'   
+    
     def get_pair_params(self):
         '''
         Sets geometrical parameters of the pair. If geometrical parameters are already stored new ones will be added as a new frame. Otherwise, Geometrical_Parameters object is created.
         '''
-        lead_nucl, lag_nucl = self.lead_nucl, self.lag_nucl
-        ori = torch.vstack([lead_nucl.origin, lag_nucl.origin]).reshape(2, 1, 3)
-        r_frames = torch.stack([lead_nucl.ref_frame, lag_nucl.ref_frame])
-
+        lead_nucl,lag_nucl = self.lead_nucl,self.lag_nucl
+        ori = torch.vstack([lead_nucl.origin,lag_nucl.origin]).reshape(2,1,3)
+        r_frames = torch.stack([lead_nucl.ref_frame,lag_nucl.ref_frame])
+        
         if self.geom_params:
-            self.geom_params.get_new_params_set(ref_frames=r_frames, origins=ori)
+            self.geom_params.get_new_params_set(ref_frames=r_frames,origins=ori)
         else:
-            self.geom_params = Geometrical_Parameters(ref_frames=r_frames, origins=ori,
-                                                      pair_params=True)
+            self.geom_params = Geometrical_Parameters(ref_frames = r_frames, origins = ori,
+                                                  pair_params=True)            
 
-    def copy(self, **kwards):
-        new = Base_Pair(lead_nucl=self.lead_nucl.copy(), lag_nucl=self.lag_nucl.copy(), radius=self.radius,
-                        charge=self.charge, eps=self.eps, dna_structure=self.dna_structure, geom_params=self.geom_params)
+    
+    def copy(self,**kwards):
+        new = Base_Pair(lead_nucl = self.lead_nucl.copy(),lag_nucl = self.lag_nucl.copy(),radius=self.radius,charge=self.charge,eps=self.eps,dna_structure=self.dna_structure,geom_params=self.geom_params)
         new.update_references()
-        for name, value in kwards.items():
-            setattr(new, name, value)
+        for name,value in kwards.items():
+            setattr(new,name,value)
         return new
-
-    def set_params(self, value, sl, attr):
-        getattr(self.geom_params, attr)[sl] = value
-
-    def _setter(self, value, attr):
-        getattr(self.storage_class, self.storage_class.get_name(attr))[self.ind] = value
-
-    def _getter(self, attr):
-        return getattr(self.storage_class, self.storage_class.get_name(attr))[self.ind]
-
+    
+    def set_params(self,value,sl,attr):
+        getattr(self.geom_params,attr)[sl] = value
+        
+    def _setter(self,value,attr):
+        getattr(self.storage_class,self.storage_class.get_name(attr))[self.ind] = value
+        
+    def _getter(self,attr):
+        return getattr(self.storage_class,self.storage_class.get_name(attr))[self.ind]
+        
     def _set_property(attr):
-        def setter(self, value): return self._setter(value, attr=attr)
-        def getter(self): return self._getter(attr=attr)
-        return property(fset=setter, fget=getter)
+        setter = lambda self,value: self._setter(value,attr=attr)
+        getter = lambda self: self._getter(attr=attr)
+        return property(fset=setter,fget=getter)
 
     lead_nucl_ind = _set_property('lead_nucl_ind')
     lag_nucl_ind = _set_property('lag_nucl_ind')
@@ -145,28 +132,28 @@ class Base_Pair:
     charge = _set_property('charge')
     epsilon = _set_property('epsilon')
     geom_params = _set_property('geom_params')
-
-    pair_params = property(fget=lambda self: self.geom_params.local_params[1], fset=lambda self, value: self.set_params(value, 1, attr='local_params'))
-    Rm = property(fget=lambda self: self.geom_params.Rm[0], fset=lambda self, value: self.set_params(value, 0, attr='Rm'))
-    om = property(fget=lambda self: self.geom_params.om[0], fset=lambda self, value: self.set_params(value, 0, attr='om'))
-
+    
+        
+    pair_params = property(fget=lambda self: self.geom_params.local_params[1],fset=lambda self,value: self.set_params(value,1,attr='local_params'))
+    Rm = property(fget=lambda self: self.geom_params.Rm[0],fset=lambda self,value: self.set_params(value,0,attr='Rm'))
+    om = property(fget=lambda self: self.geom_params.om[0],fset=lambda self,value: self.set_params(value,0,attr='om'))
+    
     @property
     def pair_name(self):
-        if isinstance(self.lead_nucl.restype, bytes):
-            return ''.join(sorted((str(self.lead_nucl.restype, "utf-8"),
-                                   str(self.lag_nucl.restype, "utf-8")))).upper()
+        if isinstance(self.lead_nucl.restype,bytes):
+            return ''.join(sorted((str(self.lead_nucl.restype,"utf-8"), 
+                               str(self.lag_nucl.restype,"utf-8")))).upper()
         else:
-            return ''.join(sorted((str(self.lead_nucl.restype),
-                                   str(self.lag_nucl.restype)))).upper()
-
+            return ''.join(sorted((str(self.lead_nucl.restype), 
+                               str(self.lag_nucl.restype)))).upper()
+    
     @property
     def lead_nucl(self):
         return self.storage_class.nucleotides_storage[self.lead_nucl_ind]
-
+    
     @property
     def lag_nucl(self):
         return self.storage_class.nucleotides_storage[self.lag_nucl_ind]
-
 
 def _get_nucl_in_pairs_ind(nucleotides_storage):
     '''
@@ -174,35 +161,31 @@ def _get_nucl_in_pairs_ind(nucleotides_storage):
     '''
 
     pair_names_check = [''.join(sorted((resname1, resname2))).upper() in ('AT', 'CG', 'AU')
-                        for (resname1, resname2) in combinations(nucleotides_storage.restypes, 2)]
-    dist = pdist(nucleotides_storage.origins.reshape(-1, 3))
+                        for (resname1,resname2) in combinations(nucleotides_storage.restypes,2)]
+    dist = pdist(nucleotides_storage.origins.reshape(-1,3))
     dist_check = dist < 4
-
-    check = [name_check and d_check for name_check, d_check in zip(pair_names_check, dist_check)]
-    if not any(check):
-        return [], []
-
-    rot_dif = [R.align_vectors(r1, r2)[0] for candidate, (r1, r2) in
-               zip(check, combinations(nucleotides_storage.ref_frames, 2)) if candidate]
-    if len(rot_dif) == 0:
-        return [], []
+    
+    check = [name_check and d_check for name_check,d_check in zip(pair_names_check,dist_check)]
+    
+    rot_dif = [R.align_vectors(r1,r2)[0] for candidate,(r1,r2) in 
+                zip(check,combinations(nucleotides_storage.ref_frames,2)) if candidate]
     rot_dif = R.concatenate(rot_dif).as_euler('zyx', degrees=True)
-    rot_dif[:, 2] += (rot_dif[:, 2] < 0) * 360
+    rot_dif[:,2] += (rot_dif[:,2] < 0) * 360
+    
+    true_pairs = classifier.predict(np.hstack([rot_dif,dist[check].reshape(-1,1)])).astype(bool)
+    
+    nucl_ind = [(i1,i2) for candidate,(i1,i2) in zip(check,combinations(range(len(nucleotides_storage)),2)) if candidate]
 
-    true_pairs = classifier.predict(np.hstack([rot_dif, dist[check].reshape(-1, 1)])).astype(bool)
-
-    nucl_ind = [(i1, i2) for candidate, (i1, i2) in zip(check, combinations(range(len(nucleotides_storage)), 2)) if candidate]
-
-    lead_ind, lag_ind = [], []
-    for true_pair, i in zip(true_pairs, nucl_ind):
+    lead_ind,lag_ind = [],[]
+    for true_pair,i in zip(true_pairs,nucl_ind):
         if true_pair:
             lead_ind.append(i[0])
             lag_ind.append(i[1])
+    
+    
+    return lead_ind,lag_ind
 
-    return lead_ind, lag_ind
-
-
-def _fix_missing_pairs(dna_structure, pairs):
+def _fix_missing_pairs(dna_structure,pairs):
     '''
     Adds missing pairs by context after classifier algorithm.
     Fixes the cases
@@ -213,69 +196,66 @@ def _fix_missing_pairs(dna_structure, pairs):
     '''
     nucleotides = dna_structure.nucleotides
     paired_nucl_ind = pairs.lead_nucl_inds + pairs.lag_nucl_inds
-    lead_to_pair = {lead_ind: pair_ind for pair_ind, lead_ind in enumerate(pairs.lead_nucl_inds)}
 
     for nucleotide in nucleotides[:-1]:
 
         if nucleotide.ind not in paired_nucl_ind and nucleotide.leading_strand:
             next_nucleotide = nucleotide.next_nucleotide
-
+            
             prev_nucleotide = nucleotide.previous_nucleotide
-
+    
             if next_nucleotide and prev_nucleotide and prev_nucleotide.ind in paired_nucl_ind and next_nucleotide.ind in paired_nucl_ind:
-                # Some nucleotides can be paired only as lagging partners, so skip them instead of raising.
-                if prev_nucleotide.ind not in lead_to_pair or next_nucleotide.ind not in lead_to_pair:
-                    continue
-                prev_pair_ind = lead_to_pair[prev_nucleotide.ind]
-                next_pair_ind = lead_to_pair[next_nucleotide.ind]
+                if prev_nucleotide.ind in pairs.lead_nucl_inds and next_nucleotide.ind in pairs.lead_nucl_inds:
+                    prev_pair_ind = pairs.lead_nucl_inds.index(prev_nucleotide.ind)
+                    next_pair_ind = pairs.lead_nucl_inds.index(next_nucleotide.ind)
 
-                lag_ind1 = pairs.lag_nucl_inds[prev_pair_ind]
-                lag_ind2 = pairs.lag_nucl_inds[next_pair_ind]
+                    lag_ind1 = pairs.lag_nucl_inds[prev_pair_ind]
+                    lag_ind2 = pairs.lag_nucl_inds[next_pair_ind]
 
-                pos_nucleotide1 = nucleotides[lag_ind1].previous_nucleotide
-                pos_nucleotide2 = nucleotides[lag_ind2].next_nucleotide
+                    pos_nucleotide1 = nucleotides[lag_ind1].previous_nucleotide
+                    pos_nucleotide2 = nucleotides[lag_ind2].next_nucleotide
 
-                if pos_nucleotide1 and pos_nucleotide2:
+                    if pos_nucleotide1 and pos_nucleotide2:
 
-                    if pos_nucleotide1.ind == pos_nucleotide2.ind:
-                        if ''.join(sorted((nucleotide.restype, pos_nucleotide1.restype))).upper() in ('AT', 'CG', 'AU'):
-                            new_base_pair = Base_Pair(pairs,
-                                                      lead_nucl=nucleotide, lag_nucl=pos_nucleotide1)
-                            new_base_pair.get_pair_params()
+                        if pos_nucleotide1.ind == pos_nucleotide2.ind:
+                            if ''.join(sorted((nucleotide.restype, pos_nucleotide1.restype))).upper() in ('AT', 'CG', 'AU'):
+                                new_base_pair = Base_Pair(pairs,
+                                                          lead_nucl=nucleotide, lag_nucl=pos_nucleotide1)
+
+                                new_base_pair.get_pair_params()
 
     return pairs.sort()
-
-
-def get_pairs(dna_structure, radius=10, charge=-2, eps=0.5):
+    
+def get_pairs(dna_structure,radius=10,charge=-2,eps=0.5):
     '''
     Runs the analysis to determine all pairs of nucleotides for a given structure.
-
+    
     Attributes:
-
+    
     **dna_structure** - DNA_Structure object to analyze.
-
+    
     **radius**, **charge**, **eps** - values of parameters to set for each pair.
-
+    
     Returns:
-
+    
     Sorted Pairs_Storage objects.
     '''
     nucleotides = dna_structure.nucleotides
-    lead_ind, lag_ind = _get_nucl_in_pairs_ind(nucleotides)
-
+    lead_ind,lag_ind = _get_nucl_in_pairs_ind(nucleotides)
+    
     ln = len(lead_ind)
-    radii, charges, epsilons = [radius]*ln, [charge]*ln, [eps]*ln
-
+    radii,charges,epsilons = [radius]*ln,[charge]*ln,[eps]*ln
+    
     geom_params = []
 
-    for ldi, lgi in zip(lead_ind, lag_ind):
-        ori = torch.vstack([nucleotides.origins[ldi], nucleotides.origins[lgi]]).reshape(2, 1, 3)
-        r_frames = torch.stack([nucleotides.ref_frames[ldi], nucleotides.ref_frames[lgi]])
-
-        geom_params.append(Geometrical_Parameters(ref_frames=r_frames, origins=ori,
+    for ldi,lgi in zip(lead_ind,lag_ind):
+        ori = torch.vstack([nucleotides.origins[lgi],nucleotides.origins[ldi]]).reshape(2,1,3)
+        r_frames = torch.stack([nucleotides.ref_frames[lgi],nucleotides.ref_frames[ldi]])
+        geom_params.append(Geometrical_Parameters(ref_frames = r_frames, origins = ori,
                                                   pair_params=True))
 
-    pairs = Pairs_Storage(Base_Pair, nucleotides, lead_ind, lag_ind, radii, charges, epsilons, geom_params)
-    pairs = _fix_missing_pairs(dna_structure, pairs)
+    pairs = Pairs_Storage(Base_Pair,nucleotides,lead_ind,lag_ind,radii,charges,epsilons,geom_params)
+
+    pairs = _fix_missing_pairs(dna_structure,pairs)
 
     return pairs

pynamod/structures/rlsp_group.py
@@ -1,26 +1,12 @@
-import io
-import shutil
-import subprocess
-import sys
-import tempfile
-
 import MDAnalysis as mda
 import numpy as np
-<<<<<<< HEAD:pynamod/structures/rlsp_group.py
 import torch
 import subprocess
 import tempfile
 from sklearn.neighbors import KNeighborsClassifier
-=======
-import pypdb
-import torch
-from sklearn.neighbors import NearestNeighbors
-
->>>>>>> 4c2722a3fcadace9ef261cd86d95e432a14f8376:pynamod/structures/protein.py
 
 
 class Real_Space_Beads_Groups:
-<<<<<<< HEAD:pynamod/structures/rlsp_group.py
     def __init__(self, n_cg_beads=None, ref_vectors=None, charges=None, masses=None,
                  radii=None, ref_pair=None, eps=1,
                  binded_dna_len=None, cg_structure=None):
@@ -31,21 +17,12 @@ class Real_Space_Beads_Groups:
         self.masses = masses
         self.radii = radii
         if isinstance(eps, torch.Tensor) or self.n_cg_beads is None:
-=======
-    def __init__(self, ref_pair=None, eps=1, binded_dna_len=None, cg_structure=None):
-        self.ref_pair = ref_pair
-        if isinstance(eps, torch.Tensor):
->>>>>>> 4c2722a3fcadace9ef261cd86d95e432a14f8376:pynamod/structures/protein.py
             self.eps = eps
         else:
             self.eps = torch.ones(n_cg_beads)*eps
 
         self.binded_dna_len = binded_dna_len
         self.cg_structure = cg_structure
-<<<<<<< HEAD:pynamod/structures/rlsp_group.py
-=======
-
->>>>>>> 4c2722a3fcadace9ef261cd86d95e432a14f8376:pynamod/structures/protein.py
 
     def save_to_h5(self, file, group_name='rlsp_0_CG_parameters', **dataset_kwards):
         group = file.create_group(group_name)
@@ -99,17 +76,12 @@ class Real_Space_Beads_Groups:
 class Protein(Real_Space_Beads_Groups):
     '''This class contains protein model as coarse grained beads with radii and charges. This class is always related to CG structure that stores positions of CG beads in All_Coords object. Init function of this class requires pdb2pqr class if initialized from mda Universe without charges.'''
 
-<<<<<<< HEAD:pynamod/structures/rlsp_group.py
     def __init__(self,n_cg_beads=50, mdaUniverse=None,  ref_vectors=None,
                  charges=None, masses=None, radii=None, ref_pair=None,
                  eps=1, binded_dna_len=None, cg_structure=None):
 
         super().__init__(n_cg_beads, ref_vectors, charges, masses,
                          radii, ref_pair, eps, binded_dna_len, cg_structure)
-=======
-    def __init__(self, mdaUniverse=None, n_cg_beads=50, ref_pair=None, eps=1, binded_dna_len=None, cg_structure=None, **kwards):
-        self.n_cg_beads = n_cg_beads
->>>>>>> 4c2722a3fcadace9ef261cd86d95e432a14f8376:pynamod/structures/protein.py
 
         if mdaUniverse:
             if hasattr(mdaUniverse.atoms, 'charges'):
@@ -117,86 +89,21 @@ class Protein(Real_Space_Beads_Groups):
             else:
                 pdb_temp = tempfile.NamedTemporaryFile(suffix='.pdb')
                 pqr_temp = tempfile.NamedTemporaryFile(suffix='.pqr')
-                # PDBWriter warns if formalcharges is missing; PDBs rarely carry that column.
-                if not hasattr(mdaUniverse.atoms, 'formalcharges'):
-                    mdaUniverse.universe.add_TopologyAttr('formalcharges')
                 mdaUniverse.atoms.write(pdb_temp.name)
-                cmd = ['pdb2pqr', '--ff=AMBER', '--log-level=CRITICAL', pdb_temp.name, pqr_temp.name]
-                if shutil.which('pdb2pqr') is None:
-                    cmd = [sys.executable, '-m', 'pdb2pqr', '--ff=AMBER', '--log-level=CRITICAL', pdb_temp.name, pqr_temp.name]
-                process = subprocess.run(cmd)
+                process = subprocess.run(['pdb2pqr', '--ff=AMBER','--log-level=CRITICAL', pdb_temp.name, pqr_temp.name])
                 self.u = mda.Universe(pqr_temp.name)
         else:
             self.u = None
-<<<<<<< HEAD:pynamod/structures/rlsp_group.py
 
     def build_model(self, dna_structure, random_state=None):
         '''Runs analysis of atomic structure. 
-=======
-        self.ref_pair = ref_pair
-        if isinstance(eps, torch.Tensor):
-            self.eps = eps
-        else:
-            self.eps = torch.ones(n_cg_beads)*eps
-
-        self.binded_dna_len = binded_dna_len
-        self.cg_structure = cg_structure
-
-        for name, value in kwards.items():
-            setattr(self, name, value)
-
-    def build_model(self, dna_structure, random_state=None):
-        '''Runs analysis of atomic structure.
->>>>>>> 4c2722a3fcadace9ef261cd86d95e432a14f8376:pynamod/structures/protein.py
 
             Attributes:
 
             **random_state** - currently not supported.'''
-        self._get_cg_centers(dna_structure, random_state)
+        self._get_cg_centers(dna_structure,random_state)
         self._get_cg_params()
-<<<<<<< HEAD:pynamod/structures/rlsp_group.py
 
-=======
-
-    def save_to_h5(self, file, group_name='protein_0_CG_parameters', **dataset_kwards):
-        group = file.create_group(group_name)
-        group.create_dataset('ref_vectors', data=self.ref_vectors, **dataset_kwards)
-        group.create_dataset('charges', data=self.charges, **dataset_kwards)
-        group.create_dataset('radii', data=self.radii, **dataset_kwards)
-        group.create_dataset('masses', data=self.masses, **dataset_kwards)
-        group.create_dataset('eps', data=self.eps, **dataset_kwards)
-        group.create_dataset('supdata', data=[self.n_cg_beads, self.ref_pair.ind, self.binded_dna_len], **dataset_kwards)
-
-    def load_from_h5(self, file, group_name='protein_0_CG_parameters'):
-        self.ref_vectors = torch.from_numpy(file[group_name]['ref_vectors'][:]).to(torch.double)
-        self.charges = torch.from_numpy(file[group_name]['charges'][:])
-        self.radii = torch.from_numpy(file[group_name]['radii'][:])
-        self.masses = torch.from_numpy(file[group_name]['masses'][:])
-        self.eps = torch.from_numpy(file[group_name]['eps'][:])
-        self.n_cg_beads = int(file[group_name]['supdata'][0])
-        self.binded_dna_len = int(file[group_name]['supdata'][2])
-        return int(file[group_name]['supdata'][1])
-
-    def get_true_pos(self, dna_structure=None, ref_om=None, ref_Rm=None):
-        if ref_om is None:
-            ref_om = dna_structure.origins[self.ref_pair.ind].detach().clone().to(self.ref_vectors.dtype)
-            ref_Rm = dna_structure.ref_frames[self.ref_pair.ind].detach().clone().to(self.ref_vectors.dtype)
-
-        return torch.matmul(self.ref_vectors.reshape(-1, 1, 3), ref_Rm.T) + ref_om
-
-    def copy(self):
-        return Protein(mdaUniverse=self.u, n_cg_beads=self.n_cg_beads, ref_pair=self.ref_pair,
-                       eps=self.eps.clone(), ref_vectors=self.ref_vectors.clone(),
-                       radii=self.radii.clone(), charges=self.charges.clone(), masses=self.masses.clone(), cg_structure=self.cg_structure,
-                       binded_dna_len=self.binded_dna_len)
-
-    def to(self, device):
-        self.charges = self.charges.to(device)
-        self.radii = self.radii.to(device)
-        self.eps = self.eps.to(device)
-        self.ref_vectors = self.ref_vectors.to(device)
-
->>>>>>> 4c2722a3fcadace9ef261cd86d95e432a14f8376:pynamod/structures/protein.py
     def _get_cg_centers(self, dna_structure, random_state):
         '''Runs optimization algorithm of CG beads positions according to procedure described in https://doi.org/10.1016/j.str.2006.10.003'''
         steps = 200*self.n_cg_beads
@@ -204,11 +111,7 @@ class Protein(Real_Space_Beads_Groups):
         non_H_atoms = self.u.select_atoms('not type H')
         max_coord = np.max(non_H_atoms.positions, axis=0)
         min_coord = np.min(non_H_atoms.positions, axis=0)
-<<<<<<< HEAD:pynamod/structures/rlsp_group.py
         origins = [np.random.uniform(min_v-10,max_v+10,(self.n_cg_beads,1))
-=======
-        origins = [np.random.uniform(min_v-10, max_v+10, (self.n_cg_beads, 1))
->>>>>>> 4c2722a3fcadace9ef261cd86d95e432a14f8376:pynamod/structures/protein.py
                    for min_v, max_v in zip(min_coord, max_coord)]
 
         origins = np.hstack(origins)
@@ -227,7 +130,6 @@ class Protein(Real_Space_Beads_Groups):
             lmbd = lmbd_0 * (lmbd_s/lmbd_0) ** (step/steps)
             eps = eps_0 * (eps_s/eps_0) ** (step/steps)
 
-<<<<<<< HEAD:pynamod/structures/rlsp_group.py
             origins = origins + eps * np.exp(-k_beads/lmbd).reshape(-1,1) * (ref_atom_r - origins)
 
         origins = torch.from_numpy(origins.reshape(-1,1,3))
@@ -241,28 +143,12 @@ class Protein(Real_Space_Beads_Groups):
         classifier.fit(self.origins.reshape(-1,3), range(self.n_cg_beads))
 
         groups = classifier.predict(self.u.atoms.positions)
-=======
-            origins = origins + eps * np.exp(-k_beads/lmbd).reshape(-1, 1) * (ref_atom_r - origins)
-
-        origins = torch.from_numpy(origins.reshape(-1, 1, 3))
-        ref_om = dna_structure.origins[self.ref_pair.ind].detach().clone().to(origins.dtype)
-        ref_Rm = dna_structure.ref_frames[self.ref_pair.ind].detach().clone().to(origins.dtype)
-        self.ref_vectors = torch.matmul((origins - ref_om), ref_Rm)
-
-    def _get_cg_params(self):
-        '''This method assigns each atom to a CG beads, than the charge of each bead is determined as a sum of charges of its atoms, and radii is defined by radius of gyration of atom groups of this CG bead.'''
-        centers = self.origins.reshape(-1, 3).detach().cpu().numpy()
-        nn = NearestNeighbors(n_neighbors=1)
-        nn.fit(centers)
-        _, groups = nn.kneighbors(self.u.atoms.positions, return_distance=True)
-        groups = groups.ravel()
->>>>>>> 4c2722a3fcadace9ef261cd86d95e432a14f8376:pynamod/structures/protein.py
         radii = np.zeros(self.n_cg_beads)
         self.charges = np.zeros(self.n_cg_beads)
         self.masses = np.zeros(self.n_cg_beads)
 
         for i in range(self.n_cg_beads):
-            sel = self.u.atoms[groups == i]
+            sel = self.u.atoms[groups==i]
             radii[i] = sel.radius_of_gyration()
             self.charges[i] = np.sum(sel.charges)
             self.masses[i] = np.sum(sel.masses)
@@ -272,7 +158,6 @@ class Protein(Real_Space_Beads_Groups):
         self.charges = torch.from_numpy(self.charges)
         self.masses = torch.from_numpy(self.masses)
 
-<<<<<<< HEAD:pynamod/structures/rlsp_group.py
     def copy(self):
         return Protein(mdaUniverse=self.u, n_cg_beads=self.n_cg_beads, ref_pair=self.ref_pair,
                        eps=self.eps.clone(), ref_vectors=self.ref_vectors.clone(),
@@ -280,12 +165,4 @@ class Protein(Real_Space_Beads_Groups):
                        cg_structure=self.cg_structure,  binded_dna_len=self.binded_dna_len)
 
     def __repr__(self):
-        return f'<Protein with {self.n_cg_beads} CG beads and linked to {self.ref_pair.ind}th pair>' 
-=======
-    def __repr__(self):
         return f'<Protein with {self.n_cg_beads} CG beads and linked to {self.ref_pair.ind}th pair>'
-
-    @property
-    def origins(self):
-        return self.get_true_pos(self.cg_structure.dna)
->>>>>>> 4c2722a3fcadace9ef261cd86d95e432a14f8376:pynamod/structures/protein.py